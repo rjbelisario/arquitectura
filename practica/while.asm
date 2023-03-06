ce5f7e90538259c25aa3c27878b6e2aff5682f0b
rjbelisario <elmorochor123@hotmail.es>
2023-03-06 10:15:10 -0400

    Update while.asm

--- practica/while.asm
+++ practica/while.asm
@@ -1,3 +1,4 @@
+	# ciclo con indice creciente
 	li $t1, 1 #i = 1
 	li $s0, 0 # variable = 0
 
@@ -8,3 +9,16 @@
 		j Mientras
 
 	Salida: #finaliza el codigo
+	
+	# ciclo con indice decreciente
+	
+	li $t1, 99 # i = 99 mas el cero 100 (n-1 veces)
+	li $s0, 0 # variable = 0
+
+	Mientras:
+		bgt $zero,$t1, Salida # Si $t1>100 hacer
+		addi $s0, $s0, 2 #sumar 2 a la variable
+		addi $t1, $t1, -1 # i++ o i--
+		j Mientras
+
+	Salida:
