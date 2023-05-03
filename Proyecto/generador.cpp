9c266dde2a35133528d004c1de6bca1fcee2a3ed
rjbelisario <elmorochor123@hotmail.es>
2023-05-03 18:19:57 -0400

    Create generador.cpp

---
+++ Proyecto/generador.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <random>
+
+using namespace std;
+
+int main() {
+    int tamano, aleatorio;
+    random_device n_aleatorio;
+    mt19937 mt(n_aleatorio());
+    uniform_int_distribution<int> dist(1, 100);
+    cout << "Ingrese un numero entero: ";
+    cin >> tamano;
+    // Genera el archivo
+    ofstream archivo("archivo.txt");
+    archivo << tamano << ','; // tamaño en la primera posicion
+    // llena el archivo
+    for (int i = 0; i < tamano; ++i) {
+        aleatorio = dist(n_aleatorio);
+        archivo << aleatorio;
+        if (i != tamano-1)
+            archivo << ",";
+    }
+    archivo << endl;
+    archivo.close();//cierra el archivo
+    getchar();
+    return 0;
+}
