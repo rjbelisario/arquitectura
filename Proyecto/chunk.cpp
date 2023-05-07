57c1ed525300e1f0983e3339985fb7123ef21792
rjbelisario <elmorochor123@hotmail.es>
2023-05-07 14:43:46 -0400

    Update chunk.cpp

--- Proyecto/chunk.cpp
+++ Proyecto/chunk.cpp
@@ -1,69 +1,61 @@
 #include <iostream>
 #include <vector>
+#include <opencv2/opencv.hpp>
 
 using namespace std;
+using namespace cv;
 
-void divide(vector<int> v, int n, vector<vector<int>>& chunks) {
+void divide(vector<int> v, int size, vector<vector<int>>& chunks) {
     int tamano_vector = v.size();
-    int tamano_particion = tamano_vector / n;
-    int r = tamano_vector % n;
-    int aux = 0;
-
-    for (int i = 0; i < n; i++) {
-        int extra = (i < r) ? 1 : 0;
-        int inicio = aux;
-        int fin = aux + tamano_particion + extra;
-        chunks.push_back(vector<int>(v.begin() + inicio, v.begin() + fin));
-        aux = fin;
+    for (int i = 0; i < tamano_vector; i += size) {
+        chunks.push_back(vector<int>(v.begin() + i, v.begin() + i + size));
     }
 }
-
-int main() {
+void cargar_datos(vector<int>& v,int& tamano){
+    int valor;
     FILE* archivo = fopen("archivo.txt", "r");
-    int tamano,valor;
     fscanf(archivo, "%d", &tamano);
-    vector<int> v;
     for (int i = 0; i < tamano; i++) {
         fscanf(archivo, "%d", &valor);
         v.push_back(valor);
     }
+    fclose(archivo);
+}
 
-//    for (int i=0; i < tamano; i++)
-//        cout << v[i] << " ";
-//    cout << endl;
-    int n,elem1,elem2, band=1;
-    while(band<0 or band>0){
-        cout << "Tamano del Vector " << v.size() << endl;
-        cout << "Ingrese cuantas veces desea partir el vector: ";
-        cin >> n;
-        vector<vector<int>> chunks;
-        divide(v, n, chunks);
-        for (int i = 0; i < chunks.size(); i++) {
-            cout << "[";
-            for (int j = 0; j < chunks[i].size(); j++) {
-                cout << chunks[i][j];
-                cout << (chunks[i].size() > j+1 ? "," : "");
-            }
-            cout << "]"<< endl;
-        }
-        cout << "Divisiones totales: " << n << " particiones" << endl;
-        cout << "Buscar elemento: ";
-        cin >> elem1;
-        for (int i = 0; i < chunks.size(); i++) {
-            for (int j = 0; j < chunks[i].size(); j++) {
-                if(chunks[i][j]==elem1)
-                    cout <<"Posicion: Vector:"<<i+1<<" Pos:"<<j+1 <<endl;
+int main() {
+    int tamano,chunk_size=4;
+    vector<int> v;
+    vector<vector<int>> chunks;
+    cargar_datos(v,tamano);
+    divide(v, chunk_size, chunks);
+
+   // Unir los chunks en una nueva imagen
+    int width = chunk_size;
+    int height = (chunks.size() + width - 1) / width;
+    Mat result(height, width, CV_32SC1);
+    int k = 0;
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            if (k < chunks.size()) {
+                result.at<int>(y, x) = chunks[k++][0];
+            } else {
+                result.at<int>(y, x) = 0;
             }
         }
-        cout << endl;
-
-        cout << "Consultar datos (VECTOR): ";
-        cin >> elem1;
-        cout << "Consultar datos (POS): ";
-        cin >> elem2;
-        cout << ((chunks[elem1-1].size() > 0 && chunks.size()>=elem1-1 && elem2-1<=chunks[elem1-1].size()) ? to_string(chunks[elem1-1][elem2-1]) : "No se encuentra dentro del vector") << endl;
-        cout << "Ingrese 0 para salir: ";
-        cin >> band;
     }
+
+    // Escalar los valores de la imagen para mostrarla correctamente
+    double min_val, max_val;
+    minMaxLoc(result, &min_val, &max_val);
+    result.convertTo(result, CV_8UC1, 255.0 / (max_val - min_val), -255.0 * min_val / (max_val - min_val));
+
+    // Mostrar la imagen resultante
+    namedWindow("Resultado", WINDOW_NORMAL);
+    imshow("Resultado", result);
+    waitKey(0);
+
+    // Guardar la imagen resultante en un archivo
+    imwrite("resultado.png", result);
+
     return 0;
 }
