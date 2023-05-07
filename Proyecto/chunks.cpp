eebe19add656583f80c3d3dc49ea59721eade99a
rjbelisario <elmorochor123@hotmail.es>
2023-05-07 18:30:36 -0400

    Update chunks.cpp

--- Proyecto/chunks.cpp
+++ Proyecto/chunks.cpp
@@ -46,23 +46,23 @@ int main() {
     // Crear la imagen a partir de los datos del vector
     int width = chunks.size(); // El ancho de la imagen es igual al número de chunks
     int height = chunks[0].size() / 3; // La altura de la imagen es igual al tamaño de cada chunk dividido por 3
-    Mat imagen(height, width, CV_8UC3, Scalar(0, 0, 0));
-    k = 0;
-    for (int x = 0; x < width; x++) {
-        for (int y = 0; y < height; y++) {
-            int i = y * 3;
-            Vec3b pixel_value(chunks[x][i+2], chunks[x][i+1], chunks[x][i]);
-            imagen.at<Vec3b>(y, x) = pixel_value;
-        }
-    }
+    //Mat imagen2(height, width, CV_8UC3, Scalar(0, 0, 0));
+    //k = 0;
+    //for (int x = 0; x < width; x++) {
+    //    for (int y = 0; y < height; y++) {
+    //        int i = y * 3;
+    //        Vec3b pixel_value(chunks[x][i+2], chunks[x][i+1], chunks[x][i]);
+    //        imagen2.at<Vec3b>(y, x) = pixel_value;
+    //    }
+    //}
 
     // Mostrar la imagen en una ventana
-    namedWindow("Imagen RGB generada", WINDOW_NORMAL);
-    imshow("Imagen RGB generada", imagen);
-    waitKey(0);
+    //namedWindow("Imagen RGB generada", WINDOW_NORMAL);
+    //imshow("Imagen RGB generada", imagen2);
+    //waitKey(0);
 
     // Guardar la imagen en un archivo
-    imwrite("imagen_generada.png", imagen);
+    //imwrite("imagen_generada.png", imagen2);
 
     // Guardar los chunks en un archivo de texto
     guardar_chunks(chunks, "chunks.txt");
