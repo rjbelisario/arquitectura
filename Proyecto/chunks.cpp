7e29104305f76a8711316a7f5f1a575b5b56a51f
rjbelisario <elmorochor123@hotmail.es>
2023-05-07 22:11:29 -0400

    Update chunks.cpp

--- Proyecto/chunks.cpp
+++ Proyecto/chunks.cpp
@@ -1,6 +1,7 @@
+#include <sstream>
 #include <fstream>
 #include <vector>
-#include <iostream>
+#include <string>
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <unistd.h>
@@ -10,13 +11,19 @@ using namespace std;
 
 void divide(vector<int> v, int n, vector<vector<int>>& chunks) {
     int tamano_vector = v.size();
+    int elementos_faltantes = n - (tamano_vector % n);
+    if (tamano_vector % n != 0) {
+        for (int i = 0; i < elementos_faltantes; i++) {
+            v.push_back(0);
+        }
+    }
 
-    for (int i = 0; i < tamano_vector; i += n) {
+    for (int i = 0; i < v.size(); i += n) {
         chunks.push_back(vector<int>(v.begin() + i, v.begin() + i + n));
     }
 }
 void cargar_datos(vector<int>& v,int& tamano, const string& filename){
-    ifstream archivo("archivo.txt");
+    ifstream archivo(filename);
     int valor;
     archivo >> tamano;
     for (int i = 0; i < tamano; i++) {
@@ -25,34 +32,36 @@ void cargar_datos(vector<int>& v,int& tamano, const string& filename){
     }
     archivo.close();
 }
-void guardar_chunks(const std::vector<std::vector<int>>& chunks, const std::string& filename) {
-    std::ofstream archivo(filename);
-    for (const std::vector<int>& chunk : chunks) {
+void guardar_chunks(const vector<vector<int>>& chunks, const string& filename) {
+    ofstream archivo(filename);
+    for (const vector<int>& chunk : chunks) {
         for (int i : chunk) {
             archivo << i << " ";
         }
-        archivo << std::endl;
+        archivo << endl;
     }
     archivo.close();
 }
-void cargar_datos_mmap(std::vector<int>& v, int& tamano, const std::string& filename) {
+void cargar_datos_mmap(vector<int>& v, int& tamano, const string& filename) {
     int fd = open(filename.c_str(), O_RDONLY);
     struct stat st;
     fstat(fd, &st);
-    tamano = st.st_size / sizeof(int);
-    int* data = static_cast<int*>(mmap(nullptr, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0));
-    for (int i = 1; i <= tamano; i++) {
-        v.push_back(data[i]);
+    const char* data = static_cast<const char*>(mmap(nullptr, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0));
+    string str(data, st.st_size);
+    istringstream iss(str);
+    int valor;
+    iss >> tamano;
+    while (iss >> valor) {
+        v.push_back(valor);
     }
-    munmap(data, st.st_size);
+    tamano = v.size();
     close(fd);
 }
 int main() {
     int tamano, n=3;
     vector<int> v;
     vector<vector<int>> chunks;
-    cargar_datos(v,tamano, "archivo.txt");
-    cout << v[0];
+    cargar_datos_mmap(v,tamano, "archivo.txt");
     divide(v, n, chunks);
     guardar_chunks(chunks, "chunks.txt");
     return 0;
