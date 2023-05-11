6448a6d0cfbf8ed7535635cd30da9d79fdd86e7a
rjbelisario <elmorochor123@hotmail.es>
2023-05-10 21:07:42 -0400

    Update chunks.cpp

--- Proyecto/chunks.cpp
+++ Proyecto/chunks.cpp
@@ -6,7 +6,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
-
+#include <iostream>
 using namespace std;
 
 void divide(vector<int> v, int n, vector<vector<int>>& chunks) {
@@ -20,6 +20,32 @@ void divide(vector<int> v, int n, vector<vector<int>>& chunks) {
 
     for (int i = 0; i < v.size(); i += n) {
         chunks.push_back(vector<int>(v.begin() + i, v.begin() + i + n));
+    }
+}
+void divide_mmap(char* data, int tamano, int n, vector<vector<int>>& chunks) {
+    int elementos_faltantes = n - (tamano % n);
+    if (tamano % n != 0) {
+        for (int i = 0; i < elementos_faltantes; i++) {
+            // agregar elementos faltantes con valor cero
+            chunks.push_back(vector<int>(n, 0));
+        }
+    }
+
+    char* endptr;
+    int* p_data = reinterpret_cast<int*>(data); // convertir el puntero a int*
+
+    for (int i = 0; i < tamano; i += n) {
+        vector<int> subvector;
+        for (int j = i + 1; j <= i + n; j++) {
+            subvector.push_back(strtol(data, &endptr, 10));
+            data = endptr + 1;
+        }
+        chunks.push_back(subvector);
+    }
+}
+void unificar_vector(const vector<vector<int>>& chunks, vector<int>& resultado) {
+    for (const auto& subvector : chunks) { // Recorre cada subvector en chunks
+        resultado.insert(resultado.end(), subvector.begin(), subvector.end()); // Agrega los elementos del subvector al resultado
     }
 }
 void cargar_datos(vector<int>& v,int& tamano, const string& filename){
@@ -42,27 +68,36 @@ void guardar_chunks(const vector<vector<int>>& chunks, const string& filename) {
     }
     archivo.close();
 }
-void cargar_datos_mmap(vector<int>& v, int& tamano, const string& filename) {
+void cargar_datos_mmap(vector<int>& v, int& tamano, char** data, const string& filename) {
     int fd = open(filename.c_str(), O_RDONLY);
     struct stat st;
     fstat(fd, &st);
-    const char* data = static_cast<const char*>(mmap(nullptr, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0));
-    string str(data, st.st_size);
-    istringstream iss(str);
-    int valor;
-    iss >> tamano;
-    while (iss >> valor) {
-        v.push_back(valor);
+    *data = static_cast<char*>(mmap(nullptr, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0));
+    char* endptr;
+    tamano = strtol(*data, &endptr, 10);
+    v.resize(tamano);
+    char* p = endptr;
+    *data = p; 
+    for (int i = 0; i < tamano; i++) {
+        v[i] = strtol(p, &endptr, 10);
+        p = endptr;
     }
-    tamano = v.size();
     close(fd);
 }
 int main() {
-    int tamano, n=3;
-    vector<int> v;
+    char* data;
+    int tamano, n=10;
+    vector<int> v,v2;
     vector<vector<int>> chunks;
-    cargar_datos_mmap(v,tamano, "archivo.txt");
-    divide(v, n, chunks);
+    cargar_datos_mmap(v,tamano, &data, "archivo.txt");
+    divide_mmap(data, tamano, n, chunks);
+    //divide(v, n, chunks);
+    //unificar_vector(chunks,v2);
+    //for (int i = 0; i < tamano; ++i) {
+    //    printf("%d ", strtol(p, &endptr, 10));
+    //    p = endptr;
+    //}
     guardar_chunks(chunks, "chunks.txt");
+    munmap(data, tamano * sizeof(int) + sizeof(int));
     return 0;
 }
