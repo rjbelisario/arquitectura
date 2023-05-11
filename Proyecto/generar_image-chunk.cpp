1267f5bfcd0d92cadd540b526f1bdee994eb52a1
rjbelisario <elmorochor123@hotmail.es>
2023-05-10 22:54:07 -0400

    Update generar_image-chunk.cpp

--- Proyecto/generar_image-chunk.cpp
+++ Proyecto/generar_image-chunk.cpp
@@ -9,7 +9,8 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 using namespace std;
-
+using namespace cv;
+ 
 void divide(vector<int> v, int n, vector<vector<int>>& chunks) {
     int tamano_vector = v.size();
     int elementos_faltantes = n - (tamano_vector % n);
@@ -18,7 +19,7 @@ void divide(vector<int> v, int n, vector<vector<int>>& chunks) {
             v.push_back(0);
         }
     }
-
+ 
     for (int i = 0; i < v.size(); i += n) {
         chunks.push_back(vector<int>(v.begin() + i, v.begin() + i + n));
     }
@@ -31,10 +32,10 @@ void divide_mmap(char* data, int tamano, int n, vector<vector<int>>& chunks) {
             chunks.push_back(vector<int>(n, 0));
         }
     }
-
+ 
     char* endptr;
     int* p_data = reinterpret_cast<int*>(data); // convertir el puntero a int*
-
+ 
     for (int i = 0; i < tamano; i += n) {
         vector<int> subvector;
         for (int j = i + 1; j <= i + n; j++) {
@@ -85,7 +86,7 @@ void cargar_datos_mmap(vector<int>& v, int& tamano, char** data, const string& f
     }
     close(fd);
 }
-
+ 
 void vector_a_imagen(const vector<int>& v, int filas, int columnas, Mat& imagen) {
     // Copiar los valores del vector en los píxeles de la imagen
     int k = 0;
@@ -98,7 +99,7 @@ void vector_a_imagen(const vector<int>& v, int filas, int columnas, Mat& imagen)
         }
     }
 }
-
+ 
 void imagen_a_vector(const Mat& imagen, vector<int>& v) {
     v.resize(imagen.rows * imagen.cols * 3);
     int k = 0;
@@ -111,35 +112,36 @@ void imagen_a_vector(const Mat& imagen, vector<int>& v) {
         }
     }
 }
-
+ 
 int main() {
     char* data;
     int tamano, n=10;
     vector<int> v,v2;
     vector<vector<int>> chunks;
-    cargar_datos_mmap(v,tamano, &data, "archivo.txt");
-    divide_mmap(data, tamano, n, chunks);
+    cargar_datos(v,tamano, "archivo.txt");
     //divide(v, n, chunks);
-    //unificar_vector(chunks,v2);
     //for (int i = 0; i < tamano; ++i) {
     //    printf("%d ", strtol(p, &endptr, 10));
     //    p = endptr;
     //}
-
+ 
     // imagen a vector
     Mat imagen = imread("imagen.png");
     imagen_a_vector(imagen, v);
-
+ 
+    //chunks y unificar chunks
+    divide(v, n, chunks);
+    guardar_chunks(chunks, "chunks.txt");
+    unificar_vector(chunks,v2);
+ 
     // vector a imagen
-    int tamano_vector = v.size();
+    int tamano_vector = v2.size();
     int filas = sqrt(tamano_vector / 3);
     int columnas = tamano_vector / (filas * 3);
-    Mat imagen(filas, columnas, CV_8UC3);
-    vector_a_imagen(v, filas, columnas, imagen);
+    Mat imagen_resultante(filas, columnas, CV_8UC3);
+    vector_a_imagen(v2, filas, columnas, imagen_resultante);
     imwrite("imagen_resultante.png", imagen_resultante);
-    
-    guardar_chunks(chunks, "chunks.txt");
-    munmap(data, tamano * sizeof(int) + sizeof(int));
+ 
+    //munmap(data, tamano * sizeof(int) + sizeof(int));
     return 0;
 }
-    
