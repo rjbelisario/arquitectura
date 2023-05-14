bda57f04ee85f8d653df26ae3bf3c0c597d273af
rjbelisario <elmorochor123@hotmail.es>
2023-05-14 12:42:17 -0400

    Update generar_image-chunk.cpp

--- Proyecto/generar_image-chunk.cpp
+++ Proyecto/generar_image-chunk.cpp
@@ -11,41 +11,6 @@
 using namespace std;
 using namespace cv;
 
-void divide(const vector<int>& v, int n, vector<vector<int>>& chunks) {
-    int tamano_vector = v.size();
-    for (int i = 0; i < tamano_vector; i += n) {
-        vector<int> chunk(v.begin() + i, v.begin() + i + n);
-        if (chunk.size() < n) {
-            chunk.resize(n, 0);
-        }
-        chunks.push_back(chunk);
-    }
-}
-void unificar_vector(const vector<vector<int>>& chunks, vector<int>& resultado) {
-    for (const auto& subvector : chunks) { // Recorre cada subvector en chunks
-        resultado.insert(resultado.end(), subvector.begin(), subvector.end()); // Agrega los elementos del subvector al resultado
-    }
-}
-void cargar_datos(vector<int>& v,int& tamano, const string& filename){
-    ifstream archivo(filename);
-    int valor;
-    archivo >> tamano;
-    for (int i = 0; i < tamano; i++) {
-        archivo >> valor;
-        v.push_back(valor);
-    }
-    archivo.close();
-}
-void guardar_chunks(const vector<vector<int>>& chunks, const string& filename) {
-    ofstream archivo(filename);
-    for (const vector<int>& chunk : chunks) {
-        for (int i : chunk) {
-            archivo << i << " ";
-        }
-        archivo << endl;
-    }
-    archivo.close();
-}
 void divide_mmap(char* data, int tamano, int n, int** chunks) {
     char* endptr;
     int num_chunks;
@@ -61,6 +26,7 @@ void divide_mmap(char* data, int tamano, int n, int** chunks) {
         *(*chunks + i) = 0; // Inicializar los elementos restantes a cero
     }
 }
+
 void guardar_chunks_mmap(int* chunks, int tamano, int n, const string& filename) {
     ofstream archivo(filename);
     int num_chunks = tamano / n + (tamano % n != 0); // Calcular el número de subvectores
@@ -77,48 +43,23 @@ void guardar_chunks_mmap(int* chunks, int tamano, int n, const string& filename)
     }
     archivo.close();
 }
-void cargar_datos_mmap(int& tamano, char* data, const string& filename) {
+
+void cargar_datos_mmap(int& tamano, char*& data, const string& filename) {
     int fd = open(filename.c_str(), O_RDONLY);
     struct stat st;
     fstat(fd, &st);
-    *data = static_cast<char*>(mmap(nullptr, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0));
+    data = static_cast<char*>(mmap(nullptr, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0));
     char* endptr;
-    tamano = strtol(*data, &endptr, 10);
-    char* p = endptr;
-    *data = p; 
+    tamano = strtol(data, &endptr, 10);
+    data = endptr; 
     close(fd);
-}
-void vector_a_imagen(const vector<int>& v, int filas, int columnas, Mat& imagen) {
-    // Copiar los valores del vector en los píxeles de la imagen
-    int k = 0;
-    for (int y = 0; y < filas; y++) {
-        for (int x = 0; x < columnas; x++) {
-            Vec3b& pixel = imagen.at<Vec3b>(y, x);
-            pixel[0] = v[k++];
-            pixel[1] = v[k++];
-            pixel[2] = v[k++];
-        }
-    }
-}
- 
-void imagen_a_vector(const Mat& imagen, vector<int>& v) {
-    v.resize(imagen.rows * imagen.cols * 3);
-    int k = 0;
-    for (int y = 0; y < imagen.rows; y++) {
-        for (int x = 0; x < imagen.cols; x++) {
-            Vec3b pixel = imagen.at<Vec3b>(y, x);
-            v[k++] = pixel[0];
-            v[k++] = pixel[1];
-            v[k++] = pixel[2];
-        }
-    }
 }
 
 void mmap_a_imagen(int* data, int filas, int columnas, Mat& imagen) {
     int k = 0;
     for (int y = 0; y < filas; y++) {
         for (int x = 0; x < columnas; x++) {
-            Vec3b& pixel = imagen.at<Vec3b>(y, x);
+            Vec3b& pixel = imagen.at<Vec3b>(Point(x, y));
             pixel[0] = *(data + k++);
             pixel[1] = *(data + k++);
             pixel[2] = *(data + k++);
@@ -130,7 +71,7 @@ void imagen_a_mmap(const Mat& imagen, int* data, int& tamano) {
     int k = 0;
     for (int y = 0; y < imagen.rows; y++) {
         for (int x = 0; x < imagen.cols; x++) {
-            Vec3b pixel = imagen.at<Vec3b>(y, x);
+            Vec3b pixel = imagen.at<Vec3b>(Point(x, y));
             *(data + k++) = pixel[0];
             *(data + k++) = pixel[1];
             *(data + k++) = pixel[2];
@@ -156,10 +97,9 @@ int main() {
 
     // imagen a mmap
     Mat imagen = imread("imagen.png");
-    imagen.convertTo(imagen, CV_8UC3);
     int filas = imagen.rows;
     int columnas = imagen.cols;
-    imagen_a_mmap(imagen, reinterpret_cast<int**>(&data), tamano);
+    imagen_a_mmap(imagen, reinterpret_cast<int*>(data), tamano);
 
     // divide mmap y guarda chunks
     divide_mmap(data, tamano, n, &chunks);
